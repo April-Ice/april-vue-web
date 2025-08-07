28c4b4cde925b49613d82b7363b1207d26b91322
April <feiyueliushang@163.com>
2025-08-07 14:15:42 +0800

    tecBorderBox

--- src/view/commonCss/boxShadow/code.js
+++ src/view/commonCss/boxShadow/code.js
@@ -77,6 +77,84 @@ let codeObj = {
         background-position: right bottom, left top, right top, left bottom;
     }
     `,
+    code4: `
+    .tecBorderBox {
+        background-color: #040334;
+        position: relative;
+        padding: 8px 6px 4px;
+        border: 3.12px solid rgba(0, 251, 251, 0.6);
+        overflow: hidden; /* 隐藏溢出光效 */
+        .trapezoid {
+            position: absolute;
+            background: #80d5ff;
+            animation: border-flash 0.5s linear forwards;
+        }
+
+        @keyframes border-flash {
+            0%,
+            100% {
+            opacity: 0.8;
+            filter: brightness(1);
+            }
+            20%,
+            80% {
+            opacity: 1;
+            filter: brightness(3);
+            // box-shadow: 0 0 4px #80d5ff;
+            box-shadow: 0 0 4px rgb(128 213 255, 0.5);
+            }
+        }
+        /* 四边依次动画 */
+        .top {
+            animation-delay: 0s;
+        }
+        .right {
+            animation-delay: 0.1s;
+        }
+        .bottom {
+            animation-delay: 0.2s;
+        }
+        .left {
+            animation-delay: 0.3s;
+        }
+
+        /* 顶部梯形 */
+        .top {
+            top: -2px;
+            left: 25%;
+            width: 50%; /* 边框长度的一半 */
+            height: 6px;
+            clip-path: polygon(0 0, 100% 0, 95% 100%, 5% 100%);
+        }
+
+        /* 右侧梯形 */
+        .right {
+            top: 25%;
+            right: -2px;
+            width: 6px;
+            height: 50%;
+            clip-path: polygon(0 5%, 100% 0, 100% 100%, 0 95%);
+        }
+
+        /* 底部梯形 */
+        .bottom {
+            bottom: -2px;
+            left: 25%;
+            width: 50%;
+            height: 6px;
+            clip-path: polygon(5% 0, 95% 0, 100% 100%, 0 100%);
+        }
+
+        /* 左侧梯形 */
+        .left {
+            top: 25%;
+            left: -2px;
+            width: 6px;
+            height: 50%;
+            clip-path: polygon(0 0, 100% 5%, 100% 95%, 0 100%);
+        }
+    }
+    `
 };
 
 export default codeObj
--- src/view/commonCss/boxShadow/index.vue
+++ src/view/commonCss/boxShadow/index.vue
@@ -4,15 +4,24 @@
       <a-alert message="点击显示代码" type="info" banner show-icon />
     </div>
 
-    <a-row :gutter="30">
-      <a-col :sm="6" :xs="12" @click="showCode(codeObj.cornerBox)">
+    <a-row  :gutter="[16,16]">
+      <a-col :sm="6" :xs="12" :span="6" class="outbox" @click="showCode(codeObj.cornerBox)">
         <div class="box cornerBox"></div>
       </a-col>
-      <a-col :sm="6" :xs="12" @click="showCode(codeObj.translucentBox)">
+      <a-col :sm="6" :xs="12" :span="6" class="outbox" @click="showCode(codeObj.translucentBox)">
         <div class="box translucentBox"></div>
       </a-col>
-      <a-col :sm="6" :xs="12" @click="showCode(codeObj.code3)">
+      <a-col :sm="6" :xs="12" :span="6" class="outbox" @click="showCode(codeObj.code3)">
         <div class="box radiusCorner"></div>
+      </a-col>
+       <a-col :sm="6" :xs="12" :span="6" class="outbox" @click="showCode(codeObj.code4)">
+        <div class="box tecBorderBox">
+          <!-- 四个装饰梯形 -->
+          <div class="trapezoid top"></div>
+          <div class="trapezoid right"></div>
+          <div class="trapezoid bottom"></div>
+          <div class="trapezoid left"></div>
+        </div>
       </a-col>
     </a-row>
     <!-- 代码弹窗 -->
@@ -44,5 +53,10 @@ export default defineComponent({
 });
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
+.outbox{
+  background: #eee;
+  border-radius: 8px;
+  padding: 10px;
+}
 </style>
